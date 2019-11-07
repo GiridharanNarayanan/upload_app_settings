85a6c070f20f7766930a4f0953fd1f20a26dd379
Giridharan Narayanan <ginara@microsoft.com>
2019-11-07 12:40:37 -0800

    Trying to update secrets in function app settings

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ const execAsyncInternal = promisify(exec);
 
 async function run() {
     try {
+
+        try {
+            await execAsyncInternal(`az --version`);
+            console.log("Az CLI is available");
+        } catch {
+            console.log("Could not find Azure CLI. Please install from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest");
+        }
+
         // `who-to-greet` input defined in action metadata file
         const nameToGreet = core.getInput('who-to-greet');
         const secrets = JSON.parse(nameToGreet);
         for (var key in secrets) {
             if (secrets.hasOwnProperty(key)) {
                 try {
-                    await execAsyncInternal(`az --version`);
-                    console.log("Az CLI is available");
+                    await execAsyncInternal(`az functionapp config appsettings set --settings ${key}=${secrets[key]} --resource-group acetest --name ssr-react-next-function`);
+                    console.log("Added the secret to app setting");
                 } catch {
-                    console.log("Could not find Azure CLI. Please install from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest");
+                    console.log("Failed to add secrets to function");
                 }
+                
             }
         }
 
